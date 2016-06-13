64c3e9575848c6968dceffcebef15f6fbe045522
Rony <Rony@Alex--Nicoles-MacBook-Pro.local>
2016-06-13 15:35:41 -0700

    deletes position absolute important from screen-reader-text in module.scss and styling for header

--- themes/Spring-University/header.php
+++ themes/Spring-University/header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme.
  *
- * @package RED_Starter_Theme
+ * @package Spring_Theme
  */
 
 ?><!DOCTYPE html>
@@ -22,7 +22,11 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h1 class="site-title screen-reader-text">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						<img class="home_img" src= "<?php echo get_template_directory_uri(); ?>/images/spring_logo_white.png" alt= "whiteLogo" />
+						<img class="menu_img" src= "<?php echo get_template_directory_uri(); ?>/images/spring_logo_white.png" alt= "menu" />
+					</h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
