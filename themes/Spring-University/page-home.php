6e1abfa941ee68c04738890cc5cdb6afb8511424
Tom Roper <Tom@MacBook-Pro.local>
2016-06-13 16:34:49 -0700

    add hero div and vimeo link to homepage

--- themes/Spring-University/page-home.php
+++ themes/Spring-University/page-home.php
@@ -9,7 +9,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-				<h1> Home Page test</h1>
+
+				<div class="home_hero">
+					<iframe src="https://player.vimeo.com/video/165338136" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+
+				</div>
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
