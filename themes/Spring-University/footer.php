4f30ae740d6b9d2c144dd909688d99236c912067
janekim0730 <janekim0730@gmail.com>
2016-06-13 16:00:55 -0700

    color and links added for the footer

--- themes/Spring-University/footer.php
+++ themes/Spring-University/footer.php
@@ -11,13 +11,14 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<i class="fa fa-facebook" aria-hidden="true"></i>
-					<i class="fa fa-twitter" aria-hidden="true"></i>
+					<div class="social-networks">
+						<a href="https://www.facebook.com/" target="_blank" alt="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="https://twitter.com/" target="_blank" alt="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					</div>
 					<p class="address">1490 W Broadway #300<br/>Vacouver, BC Canada</p>
-					<p class="operatio-hours"><span>Monday - Friday:</span>9:00am - 5:00pm (PST)</p>
-					<p class="phone-number">+1(604)440-0101</p>
-					<a href="#">help@springu.is</a>
-
+					<p class="operation-hours"><span>Monday - Friday:</span><br/>9:00am - 5:00pm (PST)</p>
+					<div class="contact-info"><span class="phone-number">+1(604)440-0101</span>
+					<a class="email-info" href="#"><span>help@springu.is</a></span></div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
