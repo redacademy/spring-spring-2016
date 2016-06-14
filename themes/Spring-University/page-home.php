2dbb99ae9dcb76b166e8ce8fdfb35c4983ced821
Tom Roper <Tom@MacBook-Pro.local>
2016-06-14 10:48:58 -0700

    home page now responsive so that vimeo doesnt show on mobile

--- themes/Spring-University/page-home.php
+++ themes/Spring-University/page-home.php
@@ -10,13 +10,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-				<div class="home_hero">
-					
+				<div class="mobile_home_hero">
+					<h1>Spring U</h1>
+				</div>
+
+				<div class="desktop_home_hero">
 					<div class="js-video [vimeo, widescreen]">
 						<iframe src="https://player.vimeo.com/video/165338136" width="100%" height="75%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
-
-
 				</div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -28,5 +29,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
