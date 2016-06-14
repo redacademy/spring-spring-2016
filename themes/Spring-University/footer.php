5971fc492f08224e6e70395d9ae45595f8e0a088
janekim0730 <janekim0730@gmail.com>
2016-06-14 11:50:38 -0700

    working on flexing the footer

--- themes/Spring-University/footer.php
+++ themes/Spring-University/footer.php
@@ -10,26 +10,30 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="line-separator"></div>
+				<!-- <div class="line-separator"></div> -->
+
 				<div class="footer-info">
 
 				<div class="spring-contact">
+
 					<div class="social-networks">
-						<div class="facebook-icon"><a href="https://www.facebook.com/SpringUni/?fref=ts" target="_blank" alt="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
-					 	<div class="twitter-icon"><a href="https://twitter.com/SpringU_is" target="_blank" alt="twitter"><i class="fa fa-twitter" aria-hidden="true"></a></i></div>
+						<span class="facebook-icon"><a href="https://www.facebook.com/SpringUni/?fref=ts" target="_blank" alt="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></span>
+						<span class="twitter-icon"><a href="https://twitter.com/SpringU_is" target="_blank" alt="twitter"><i class="fa fa-twitter" aria-hidden="true"></a></i></span>
 					</div>
 
 					<div class="office-info">
 						<p class="address">1490 W Broadway #300<br/>Vacouver, BC Canada</p>
 						<p class="operation-hours"><span>Monday - Friday:</span><br/>9:00am - 5:00pm (PST)</p>
 					</div>
-				</div>
-
 
 					<div class="contact-info">
 						<span class="phone-number">+1 (604) 440-0101</span>
 						<a class="email-info" href="mailto:help@springu.is"><span>help@springu.is</a></span>
 					</div>
+
+
+
+				</div>
 
 				</div><!-- .footer-info -->
 			</footer><!-- #colophon -->
