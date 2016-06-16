d76a1e57d2a887f2b907b1c29721c018ac62fe28
janekim0730 <janekim0730@gmail.com>
2016-06-15 17:04:25 -0700

    404 page, working on styling

--- themes/Spring-University/404.php
+++ themes/Spring-University/404.php
@@ -7,45 +7,22 @@
  * @package RED_Starter_Theme
  */
 
-get_header(); ?>
+ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
+					<h2 class="page-title">error</h2>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
 
-					<?php get_search_form(); ?>
+					<p>Something failed to load properly.</p>
+					<p>"...It is better to be a failure at something you love than to be a success at something you hate."<span>-George Burns</span></p>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
+					<a href="<?php echo get_option('home'); ?>"/><button>Go back home</a></button>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
