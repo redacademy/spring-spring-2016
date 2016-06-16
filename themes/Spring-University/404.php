e390f1a0715c032bcffe0ebc27af82cf51a54371
janekim0730 <janekim0730@gmail.com>
2016-06-15 21:54:46 -0700

    styling on 404 page, create mixins for button and mobile header

--- themes/Spring-University/404.php
+++ themes/Spring-University/404.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
+ * Template Name: 404 pages (not found)
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package RED_Starter_Theme
+ * @package Spring_Theme
  */
 
  get_header(); ?>
@@ -13,20 +13,21 @@
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h2 class="page-title">error</h2>
+				<header class="error-header">
+					<h2 class="error-title">error</h2>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
+				<div class="error-content">
 
 					<p>Something failed to load properly.</p>
-					<p>"...It is better to be a failure at something you love than to be a success at something you hate."<span>-George Burns</span></p>
+					<p>"...It is better to be a failure at something you love than to be a success at something you hate."<br/><span>-George Burns</span></p>
 
-					<a href="<?php echo get_option('home'); ?>"/><button>Go back home</a></button>
+					<div class="button-container">
+						<a class="go-home-button" href="<?php echo get_option('home'); ?>"/>Go back home</a>
+					</div>
+
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_footer(); ?>
