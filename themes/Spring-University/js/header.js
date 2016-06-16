14021d87d7ef53f3365f41219172d0ba07a1d63f
Rony <Rony@Alex--Nicoles-MacBook-Pro.local>
2016-06-16 15:48:42 -0700

    changes to navbar

--- themes/Spring-University/js/header.js
+++ themes/Spring-University/js/header.js
@@ -5,12 +5,20 @@
  *
  */
 
- (function($) {
+(function($) {
 
-   $( ".menu-toggle" ).click(function() {
-     $( ".hamburger" ).toggle();
-     $( ".x-mark" ).toggle();
-     $( ".menu-spring_main_menu-container" ).toggle();
+  $( ".menu-toggle" ).click(function() {
+    $( ".hamburger" ).toggle();
+    $( ".x-mark" ).toggle();
+    $( ".menu-spring_main_menu-container" ).toggle();
+  });
+
+  $( window ).resize(function() {
+    if ($(window).width() > 920) {
+      $(".menu-spring_main_menu-container").html(ul)
+    } else {
+      $(".menu-spring_main_menu-container").html("");
+    }
    });
 
  })( jQuery );
