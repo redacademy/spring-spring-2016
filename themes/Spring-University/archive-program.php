9d589406adc4fde049c862c1e00c9730ab3253a6
janekim0730 <janekim0730@gmail.com>
2016-06-17 15:53:24 -0700

    working on styling archive programs page

--- themes/Spring-University/archive-program.php
+++ themes/Spring-University/archive-program.php
@@ -10,18 +10,31 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-				<header class="page-header">
-					<h2>what we offer</h2>
+				<header>
+					<h2 class="section_header">what we offer</h2>
 				</header><!-- .page-header -->
 
+<div class="program-container">
 				<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
+             <div class="single-container">
+							 <a href="<?php echo esc_url( get_permalink() ); ?>">
 
-						<?php
-							get_template_part( 'template-parts/content' );
-						?>
-           <?php echo CFS()->get( 'program_tuition' ); ?>
+							<div class="program-image"><img src="<?php echo CFS()->get( 'program_image' ); ?>"/></div>
+
+							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<?php the_title( sprintf( '<h2 class="program-title">', '</h2>' ) ); ?>
+							</div><!-- #post-## -->
+
+							<?php the_excerpt(); ?>
+
+						  <div class="program-tuition"><p><?php echo CFS()->get( 'program_tuition' ); ?></p>
+							<span><?php echo CFS()->get( 'program_onetime_tuition' ); ?></span></div>
+
+						</a>
+					</div>
 					<?php endwhile; ?>
+</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
