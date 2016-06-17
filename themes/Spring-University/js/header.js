41a996f2d039d367ffb836d4fb748fa1534bc1a9
Rony <Rony@Alex--Nicoles-MacBook-Pro.local>
2016-06-17 09:14:53 -0700

    navbar is complete

--- themes/Spring-University/js/header.js
+++ themes/Spring-University/js/header.js
@@ -6,19 +6,9 @@
  */
 
 (function($) {
-
   $( ".menu-toggle" ).click(function() {
     $( ".hamburger" ).toggle();
     $( ".x-mark" ).toggle();
     $( ".menu-spring_main_menu-container" ).toggle();
   });
-
-  $( window ).resize(function() {
-    if ($(window).width() > 920) {
-      $(".menu-spring_main_menu-container").html(ul)
-    } else {
-      $(".menu-spring_main_menu-container").html("");
-    }
-   });
-
- })( jQuery );
+})( jQuery );
