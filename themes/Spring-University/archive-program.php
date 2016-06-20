84baa255074cccaa32837f44c5bb97e8a9fdbe44
janekim0730 <janekim0730@gmail.com>
2016-06-20 16:25:56 -0700

    styling on archive program page

--- themes/Spring-University/archive-program.php
+++ themes/Spring-University/archive-program.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-				<header>
+				<header class="program-header">
 					<h2 class="section_header">what we offer</h2>
 				</header><!-- .page-header -->
 
@@ -19,18 +19,15 @@ get_header(); ?>
 					<?php while ( have_posts() ) : the_post(); ?>
              <div class="single-container">
 							 <a href="<?php echo esc_url( get_permalink() ); ?>">
-
 							<div class="program-image"><img src="<?php echo CFS()->get( 'program_image' ); ?>"/></div>
-
 							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 								<?php the_title( sprintf( '<h2 class="program-title">', '</h2>' ) ); ?>
 							</div><!-- #post-## -->
-
 							<?php the_excerpt(); ?>
 
-						  <div class="program-tuition"><?php echo CFS()->get( 'program_tuition' ); ?></div>
+              <div class="program-keywords"><?php echo CFS()->get( 'program_keywords' ); ?></div>
+						  <div class="program-tuition"><span>$</span><span class="tuition-amount"><?php echo CFS()->get( 'program_tuition' ); ?></span></div>
 							<div class="program-onetime-tuition"><?php echo CFS()->get( 'program_onetime_tuition' ); ?></div>
-
 						</a>
 					</div>
 					<?php endwhile; ?>
