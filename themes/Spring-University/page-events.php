9b7a0f426078b00ec2d414f1dd040c3b897d6902
Tom Roper <Tom@MacBook-Pro.local>
2016-06-21 14:36:59 -0700

    create page-events, add _events.scss and add the import for events to page-templates.scss

---
+++ themes/Spring-University/page-events.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+			<h1 class="events_h1">events heyooooo!</h1>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
