23450792a23b72cdcce227af958628c00c84ad79
Tom Roper <Tom@MacBook-Pro.local>
2016-06-21 16:06:53 -0700

    setting up blog page template

--- themes/Spring-University/page.php
+++ themes/Spring-University/page.php
@@ -10,6 +10,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<section class="page_section_header">
+				<h1>Page title</h1>
+			</section>
+
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
