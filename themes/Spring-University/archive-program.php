661d018698fb01a721303db0b66b55f69213ac30
janekim0730 <janekim0730@gmail.com>
2016-06-21 09:22:02 -0700

    working on putting contents for single program page

--- themes/Spring-University/archive-program.php
+++ themes/Spring-University/archive-program.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Programs Template
+ * Template Name: Archive Programs
  *
  * @package Spring_Theme
  */
@@ -9,14 +9,14 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		
+
 					<h2 class="program-offer">what we offer</h2>
 
 <div class="program-container">
 				<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
              <div class="single-container">
-							 <a href="<?php echo esc_url( get_permalink() ); ?>">
+							 <a class="programs-link" href="<?php echo esc_url( get_permalink() ); ?>">
 							<div class="program-image"><img src="<?php echo CFS()->get( 'program_image' ); ?>"/></div>
 							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 								<?php the_title( sprintf( '<h2 class="program-title">', '</h2>' ) ); ?>
--- themes/Spring-University/single-program.php
+++ themes/Spring-University/single-program.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * Template Name: Single Program
  *
- * @package RED_Starter_Theme
+ * @package Spring_Theme
  */
 
 get_header(); ?>
@@ -10,22 +10,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<div class="program-content">
+			<?php the_content(); ?>
+		</div>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+		<h2 class="sections-header">What you learn</h2>
 
 
-		<?php endwhile; // End of the loop. ?>
-
-		<div class="program-description"><?php echo CFS()->get( 'program_description' ); ?></div>
-		<h2 class="section_header">What you learn</h2>
-		<div class="program-syllabus"><?php echo CFS()->get( 'program_syllabus' ); ?></div>
-		<h2 class="section_header">Program Dates</h2>
+		<div class="program-learnings"><?php echo CFS()->get( 'program_learnings' ); ?></div>
+		<h2 class="sections-header">Program Dates</h2>
 		<div class="program-dates"><?php echo CFS()->get( 'program_dates' ); ?></div>
 		<div class="program-info"><?php echo CFS()->get( 'program_info' ); ?></div>
-		<h2 class="section_header">Your Instructors</h2>
+		<h2 class="sections-header">Your Instructors</h2>
 		<p>need to create another custom field called instructors and use plugin post2post to link!!!</p>
-		<h2 class="section_header">Tuition</h2>
+		<h2 class="sections-header">Tuition</h2>
 		<div class="program-tuition"><?php echo CFS()->get( 'program_tuition' ); ?></div>
 		<div class="program-onetime-tuition"><?php echo CFS()->get( 'program_onetime_tuition' ); ?></div>
 
