3f74a8486ffdeed243d50488e4a728b5a8938725
janekim0730 <janekim0730@gmail.com>
2016-06-22 15:35:49 -0700

    styling on tuition section with little icons

--- themes/Spring-University/archive-program.php
+++ themes/Spring-University/archive-program.php
@@ -10,26 +10,27 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-					<h2 class="program-offer">what we offer</h2>
-
-<div class="program-container">
-				<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-             <div class="single-container">
-							 <a class="programs-link" href="<?php echo esc_url( get_permalink() ); ?>">
+	  <h2 class="program-offer">what we offer</h2>
+		<div class="program-container">
+				<?php
+				$args = array( 'post_type' => 'program',
+												'posts_per_page' => 4,
+												'order' => 'ASC');
+				$program_post = get_posts( $args );
+				?>
+				<?php foreach ( $program_post as $post ) : setup_postdata( $post ); ?>
+					<div class="single-container">
+						<a class="programs-link" href="<?php echo esc_url( get_permalink() ); ?>">
 							<div class="program-image"><img src="<?php echo CFS()->get( 'program_image' ); ?>"/></div>
 							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-								<?php the_title( sprintf( '<h2 class="program-title">', '</h2>' ) ); ?>
-							</div><!-- #post-## -->
-							<?php the_excerpt(); ?>
-
-              <div class="program-keywords"><?php echo CFS()->get( 'program_keywords' ); ?></div>
-						  <div class="program-tuition"><span>&dollar;</span><span class="tuition-amount"><?php echo CFS()->get( 'program_tuition' ); ?></span><span class="frequency"><?php echo CFS()->get( 'frequency' ); ?></span></div>
-							<div class="program-onetime-tuition"><?php echo CFS()->get( 'program_onetime_tuition' ); ?></div>
+								<?php the_title( sprintf( '<h2 class="program-title">', '</h2>' ) ); ?></div>
+								<div class="program-keywords"><?php echo CFS()->get( 'program_keywords' ); ?></div>
+								<div class="program-tuition"><span>&dollar;</span><span class="tuition-amount"><?php echo CFS()->get( 'program_tuition' ); ?></span><span class="frequency"><?php echo CFS()->get( 'frequency' ); ?></span></div>
+								<div class="program-onetime-tuition"><?php echo CFS()->get( 'program_onetime_tuition' ); ?></div>
 						</a>
 					</div>
-					<?php endwhile; ?>
-</div>
+					<?php endforeach; wp_reset_postdata(); ?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
