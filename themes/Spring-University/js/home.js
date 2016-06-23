3237a4691d5b5213adf71e021d8fc9bd5bd27376
Tom Roper <Tom@MacBook-Pro.local>
2016-06-22 23:54:07 -0700

    autoplay enabled

--- themes/Spring-University/js/home.js
+++ themes/Spring-University/js/home.js
@@ -2,7 +2,7 @@
 
    
    //video url variable, set autoplay to =1 to autoplay. 
-   var videoUrl = "<iframe id=\"vimeo_player\" src=\"https://player.vimeo.com/video/165338136?portrait=0&title=0&autoplay=0&byline=0&loop=1\" width=\"100%\" height=\"75%\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+   var videoUrl = "<iframe id=\"vimeo_player\" src=\"https://player.vimeo.com/video/165338136?portrait=0&title=0&autoplay=1&byline=0&loop=1\" width=\"100%\" height=\"75%\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
    
    // on page load over 640px video will load. under and it will be mobile view.
    if ($(window).width() > 640) {
