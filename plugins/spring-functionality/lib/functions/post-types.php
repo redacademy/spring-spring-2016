fb6f9d21f8903fe782c9b9802ef22c666b619eb1
Tom Roper <Tom@MacBook-Pro.local>
2016-06-24 14:57:26 -0700

    editing post types to add events

--- plugins/spring-functionality/lib/functions/post-types.php
+++ plugins/spring-functionality/lib/functions/post-types.php
@@ -124,3 +124,59 @@ function spring_instructors() {
 
 }
 add_action( 'init', 'spring_instructors', 0 );
+
+// Register Custom Post Type
+function spring_events_post_type() {
+
+	$labels = array(
+		'name'                  => 'Events',
+		'singular_name'         => 'Event',
+		'menu_name'             => 'Event Types',
+		'name_admin_bar'        => 'Event Type',
+		'archives'              => 'Event Archives',
+		'parent_item_colon'     => 'Parent Event:',
+		'all_items'             => 'All Events',
+		'add_new_item'          => 'Add New Event',
+		'add_new'               => 'Add Event',
+		'new_item'              => 'New Event',
+		'edit_item'             => 'Edit Event',
+		'update_item'           => 'Update Event',
+		'view_item'             => 'View Event',
+		'search_items'          => 'Search Event',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Event',
+		'uploaded_to_this_item' => 'Uploaded to this Event',
+		'items_list'            => 'Events list',
+		'items_list_navigation' => 'Events list navigation',
+		'filter_items_list'     => 'Filter Events list',
+	);
+	$args = array(
+		'label'                 => 'Event',
+		'description'           => 'events post type',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-tickets-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => 'events',		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'events', $args );
+
+}
+add_action( 'init', 'spring_events_post_type', 0 );
+
